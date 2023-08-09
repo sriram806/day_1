c9db2bb5d38b30ef6a95a100dc13ce4309f06346
Sriram <123923949+sriram806@users.noreply.github.com>
2023-08-09 20:45:22 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+name = 'Garry'
+name = 'Roger'
+name = 'Jinab'
+print(name)
+
+
+# In[ ]:
+
+
+# MULTIPLE VARIATIONS
+gary_age = roger_age = tom_age = 24
+print("Gary's age = ",gary_age)
+print("Roger's age = ",roger_age)
+print("Tom's age = ",tom_age)
+
+
+# In[3]:
+
+
+# creating string varaiables 
+name = "Charles"
+print(name)
+type (name)
+
+
+# In[5]:
+
+
+#IMPLICIT CONVERSION
+sales_price_per_unit = 4.5
+num_of_units_purchased = 5
+total_price = 0
+total_price = sales_price_per_unit * num_of_units_purchased
+print(total_price)
+type(total_price)
+
+
+# In[16]:
+
+
+#explicit conversion
+price = 250
+tax = "2.5"
+total_price = price + float(tax)
+print(total_price)
+
+
+# In[15]:
+
+
+#type casting
+unit_price = 52.8
+str(unit_price)
+print(unit_price)
+hasSpeed, isLivingThing = 1,0
+bool(hasSpeed)
+bool(isLivingThing)
+
+
+# In[20]:
+
+
+#print function
+print("Hello World")
+print("Hello","Mr.Thomas")
+print("/n")
+computer_accessories = ("CPU","Mouse","Keyboard")
+print(computer_accessories)
+
+
+# In[21]:
+
+
+#concatenation
+print("Python" + "is fun")
+
+
+# In[22]:
+
+
+#concatenation with cast typing
+print("Sam's age is" + str(25))
+
+
+# In[ ]:
+
+
+
+
